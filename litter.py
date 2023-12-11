99d0271c3fd027980af50ee5b07792c1329e5984
Topher McKee <tophermckee@gmail.com>
2023-12-11 10:41:13 -0600

    move scooping to calendar

--- litter.py
+++ litter.py
@@ -1,6 +1,11 @@
-import os, pprint, datetime, logging
+import os, pprint, datetime, logging, sys
 from todoist_api_python.api import TodoistAPI
 from pathlib import Path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from googleapiclient.errors import HttpError
 import __main__
 
 pp = pprint.PrettyPrinter(indent=2)
@@ -13,19 +18,68 @@ logging.basicConfig(
     filemode='a'
 )
 
+if sys.platform == 'darwin':
+    token_path = '/Users/tophermckee/todoist_scripts/token.json'
+elif sys.platform == 'linux':
+    token_path = '/home/data_admin/todoist_scripts/token.json'
+
+if sys.platform == 'darwin':
+    service_account_path = '/Users/tophermckee/todoist_scripts/calendar-api-service-account.json'
+elif sys.platform == 'linux':
+    credentials_path = '/home/data_admin/todoist_scripts/calendar-api-service-account.json'
+
+
+def google_auth_flow():
+    SCOPES = [
+        "https://www.googleapis.com/auth/calendar"
+    ]
+
+    calendar_creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(token_path):
+        calendar_creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not calendar_creds or not calendar_creds.valid:
+        if calendar_creds and calendar_creds.expired and calendar_creds.refresh_token:
+            calendar_creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                service_account_path, SCOPES)
+            calendar_creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(token_path, 'w') as token:
+            token.write(calendar_creds.to_json())
+
+    return calendar_creds
+
 if __name__ == '__main__':
     auth_token = os.getenv('TODOIST_AUTH_TOKEN')
     api = TodoistAPI(auth_token)
 
-    logging.info(f"{auth_token=}")
+    calendar_creds = google_auth_flow()
+
+    family_calendar_id = '6d757d0674c5e2f9850aa300bad6aa0bf235fedb5a636aba80474050db297fe5@group.calendar.google.com'
     
-    task_contents = []
+    calendar_contents = []
+
+    try:
+        page_token = None
+        while True:
+            service = build('calendar', 'v3', credentials=calendar_creds)
+            events = service.events().list(calendarId=family_calendar_id, pageToken=page_token).execute()
+            for event in events['items']:
+                calendar_contents.append(event['summary'])
+            page_token = events.get('nextPageToken')
+            if not page_token:
+                break
+    except Exception as err:
+        logging.error(f"Error getting cal events: {err}", exc_info=True)
     
     try:
         all_tasks = api.get_tasks(filter="#🏠 Chores")
-        
-        for task in all_tasks:
-            task_contents.append(task.content)
         
         for task in all_tasks:
             
@@ -39,17 +93,47 @@ if __name__ == '__main__':
                 if scoop_due_date > datetime.date.today():
                     logging.info(f"{litter_due_date=} {scoop_due_date=}")
                     logging.info('scoop date is in the future')
-                    if 'Scoop litter' in task_contents:
-                        logging.info('Scoop task already in task list')
+                    
+                    if 'Scoop cat litter' in calendar_contents:
+                        logging.info('Scoop cal event already on calendar')
                     else:
-                        logging.info('Scoop task not in task list')
-                        task = api.add_task(
-                            content="Scoop litter",
-                            due_string=f"{scoop_due_datetime.strftime('%B %d at %-I %p')}",
-                            due_lang="en",
-                            priority=2,
-                            project_id=2300556566,
-                        )
+                        logging.info('Scoop cal event not on calendar')
+                        try:
+                            event = {
+                                'summary': 'Scoop cat litter',
+                                'location': '920 Poeyfarre St Unit 115, New Orleans, LA 70130',
+                                'description': 'Courtney so lovingly scoops our sweet babies\'s poo',
+                                'start': {
+                                    'dateTime': f"{scoop_due_datetime.strftime('%Y-%m-%d')}T20:00:00-06:00",
+                                    'timeZone': 'America/Chicago',
+                                },
+                                'end': {
+                                    'dateTime': f"{scoop_due_datetime.strftime('%Y-%m-%d')}T20:30:00-06:00",
+                                    'timeZone': 'America/Chicago',
+                                },
+                                # 'recurrence': [
+                                #     'RRULE:FREQ=DAILY;COUNT=2'
+                                # ],
+                                # 'attendees': [
+                                #     {'email': 'lpage@example.com'},
+                                #     {'email': 'sbrin@example.com'},
+                                # ],
+                                'reminders': {
+                                    'useDefault': True,
+                                    # 'overrides': [
+                                    #     {'method': 'email', 'minutes': 24 * 60},
+                                    #     {'method': 'popup', 'minutes': 10},
+                                    # ],
+                                    },
+                                }
+
+                            event = service.events().insert(calendarId=family_calendar_id, body=event).execute()
+                            
+                            logging.info(f"Event created: {event.get('htmlLink')}")
+
+                        except Exception as err:
+                            logging.error(f"Error getting cal events: {err}", exc_info=True)
+                            
                 else:
                     logging.info(f"{litter_due_date=} {scoop_due_date=}")
                     logging.info('scoop date is in the past')
